93715df652834d98c634cf26ffb3a606b405f7aa
TxDevil <45487816+TxDevil@users.noreply.github.com>
2018-12-10 11:36:37 +0100

    Nieuwe Website
    Alle bestanden van mijn nieuwe website (word nog geupdate)

---
+++ Nieuwe Website/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Contactpagina</title>
+    <!-- Bootstrap kernel CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles voor deze template. -->
+    <link href="libs/css/navbar.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <!-- Navigatie -->
---
+++ Nieuwe Website/navbar.php
@@ -0,0 +1,36 @@
+<nav class="col-md-0 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="products.php">
+                  <span data-feather="home"></span>
+                  Hele assortiment <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Knalvuurwerk.php">
+                  <span data-feather="Knalvuurwerk"></span>
+                  Knalvuurwerk
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Siervuurwerk.php">
+                  <span data-feather="Siervuurwerk"></span>
+                  Siervuurwerk
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Kindervuurwerk.php">
+                  <span data-feather="Kindervuurwerk"></span>
+                  Kindervuurwerk
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Veiligheid.php">
+                  <span data-feather="Bescherming"></span>
+                  Veiligheidsartikelen
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
